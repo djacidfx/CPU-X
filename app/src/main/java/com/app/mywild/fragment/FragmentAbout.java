e0eb5f2805daf2a064aca5a4f2ee00c1cf2924f8
David Burns <djacidburn1877@gmail.com>
2023-12-02 20:34:01 -0500

    update

--- app/src/main/java/com/app/mywild/fragment/FragmentAbout.java
+++ app/src/main/java/com/app/mywild/fragment/FragmentAbout.java
@@ -1,5 +1,7 @@
 package com.app.mywild.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
@@ -8,6 +10,7 @@ import android.view.ViewGroup;
 
 import com.app.mywild.R;
 import com.app.mywild.tools.Utils;
+import android.widget.Button;
 
 public class FragmentAbout extends Fragment {
 
@@ -15,13 +18,25 @@ public class FragmentAbout extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_about, container, false);
 
-        rootView.findViewById(R.id.bt_read).setOnClickListener(new View.OnClickListener() {
+        Button readMoreButton = rootView.findViewById(R.id.bt_read);
+        Button websiteButton = rootView.findViewById(R.id.bt_read2);
+
+        readMoreButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Utils.rateAction(getActivity());
             }
         });
 
+        websiteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://donate.stripe.com/aEU8yle0n8c7b96cMN"; // Replace with the desired website URL
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
 
         return rootView;
     }
